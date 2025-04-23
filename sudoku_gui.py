8411dc83742e7b007b58b2ace876f02c720f1704
Brooke Clayton <bclayton1@ufl.edu>
2025-04-23 10:13:06 -0400

    began creating Board class methods

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -290,25 +290,25 @@ class Board:
 
         pygame.display.update()
 
-    def select(self, row, col):
-
-    def click(self, row, col):
-
-    def clear(self):
-
-    def sketch(self, value):
-
-    def place_number(self, value):
-
-    def reset_to_original(self):
-
-    def is_full(self):
-
-    def update_board(self):
-
-    def find_empty(self):
-
-    def check_board(self):
+    # def select(self, row, col):
+    #
+    # def click(self, row, col):
+    #
+    # def clear(self):
+    #
+    # def sketch(self, value):
+    #
+    # def place_number(self, value):
+    #
+    # def reset_to_original(self):
+    #
+    # def is_full(self):
+    #
+    # def update_board(self):
+    #
+    # def find_empty(self):
+    #
+    # def check_board(self):
 
 
 
